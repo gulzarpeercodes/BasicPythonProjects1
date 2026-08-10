fa5396f64c2aee40ae8ca5d4551c80654cb23a30
rayaanzargar <your-email@example.com>
2026-08-10 19:53:23 +0530

    Initial commit

---
+++ Brand name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to this amazing Brand Name Generator!")
+city = input("What is the name of the city you grew up in?\n")
+word = input("What is your favourite english word?\n")
+brand_name = city + " " + word
+print("Your Brand Name could be: " + brand_name )
