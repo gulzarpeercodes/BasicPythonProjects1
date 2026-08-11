7f8a8ecd9c538341c4fce226c3c0f107e11bfe9f
gulzarpeercodes <gulzarpeercodes@gmail.com>
2026-08-11 13:06:54 +0530

    Add files via upload

---
+++ Even Odd.py
@@ -0,0 +1,6 @@
+print("Welcome to EVEN and ODD checker")
+number_by_user = int(input("Write down your number: "))
+if number_by_user % 2 == 0:
+    print(f"{number_by_user} is an even number!")
+else:
+    print(f"{number_by_user} is an odd number!")
